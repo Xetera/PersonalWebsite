309dca9816d7a2530ad8fd2762b1046c0ca64d6e
Ali <mythologyali@gmail.com>
2017-12-18 00:59:32 -0800

    fetching pictures from Curiousity API properly

--- Rover/Gallery.php
+++ Rover/Gallery.php
@@ -12,7 +12,6 @@ $rover = new Gallery("FHAZ", 1000);
 
 
 if (isset($_POST['pageNumber'])){
-    echo $_POST['pageNumber'].getallheaders();
     $rover->contactAPI(1);
 }
 
@@ -26,6 +25,7 @@ class Gallery
     // we might need some sort of const defition to make setting camera names easier
     private $activeCam;
     private $sol;
+    private $activePics = [];
     function __construct($cam, $sol) {
         $this->activeCam = $cam;
         $this->sol = $sol;
@@ -40,17 +40,27 @@ class Gallery
         * so we probably need to query the server and cache the results before the user reaches
         * the end of the page so it looks instant */
 
+        // cURL is unnecessarily complicated but for some reason pecl HTTP requests extension
+        // doesn't seem to be working
         $c = curl_init();
         // not sure what sol is so I'm leaving it as the example in the description
-        $url = "$this->baseURL" . 'sol=1000&' . "page={$pageNumber}" . $this->activeCam;
+        $url = "$this->baseURL" . 'sol=1000&' . "page={$pageNumber}" . $this->activeCam . $this->apiKey;
         curl_setopt($c, CURLOPT_URL, $url);
         curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
         // need a way to confirm the http response before we echo the result
         $output = curl_exec($c);
         curl_close($c);
-        $json = json_decode($output);
-        echo "Loading...";
-        echo $output;
+        $json = json_decode($output, JSON_PRETTY_PRINT);
+        // loop array
+        foreach ($json as $item){
+            // loop dict
+            foreach ($item as $key => $value){
+                array_push($this->activePics, $item[$key]['img_src']);
+            }
+        }
+        echo print_r($this->activePics);
+        //echo "Loading...";
+        //echo $json;
 
     }
 }
